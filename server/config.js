e5018a61d2f7fd265098d04c370adf782f34cd99
aji1904 <38060397+aji1904@users.noreply.github.com>
2021-03-14 23:08:47 +0700

    add routes

--- server/config.js
+++ server/config.js
@@ -6,9 +6,3 @@ var conn = mysql.createConnection({
     database : 'sik'
 })
 
-conn.connect()
-conn.query('select * from akun_bayar', function (err, rows, field) {
-    if(err) throw console.error(err);
-    console.log('this data', rows[].nama_bayar)
-})
-conn.end()
--- server/server.js
+++ server/server.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const app = express()
 const port = 4000
-import config from 'config'
+const config = require('./config')
 
 app.get('/', (req, res) => {
     res.send('hello world')
 })
 
-app.get
+app.get('/akun_bayar', (req, res) => {
+    conn.connect()
+    conn.query('select * from akun_bayar', function (err, rows, field) {
+        if(err) throw console.error(err);
+        console.log('this data', rows.nama_bayar)
+    })
+    conn.end()
+});
 
 app.listen (port, () => {
     console.log(`server running port : ${port}`)
